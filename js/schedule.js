18c7ae13ad6f98158099a6bdc779999ec579b42d
Malte Ubl <malte.ubl@gmail.com>
2019-05-25 06:03:10 -0700

    Make it work.
    However, strict adherence to schedule isn't what we actually want.
    Should probably only automatically transition into session, not out of session.

--- js/schedule.js
+++ js/schedule.js
@@ -1,32 +1,46 @@
 navigator.serviceWorker.register("/sw.js");
 
+var overriddenTimeOfDay = null;
+
 (function() {
   function getTimeOfDay() {
-    return 11.3;
+    if (overriddenTimeOfDay) {
+      return overriddenTimeOfDay;
+    }
     var d = new Date();
     return Number(d.getHours() + "." + d.getMinutes());
   }
 
   var lastChecksum = "";
+  var schedule;
   var trackId = document.documentElement.getAttribute("trackid");
   if (!trackId) {
     throw new Error(`Can't find trackid`);
   }
   poll("https://2019.jsconf.eu/schedule.json", function(url, text) {
-    var schedule = JSON.parse(text);
-    if (lastChecksum == schedule.info.checksum) {
+    var s = JSON.parse(text);
+    if (lastChecksum == s.info.checksum) {
       console.info("Schedule checksum unchanged");
       return;
     }
-    lastChecksum = schedule.info.checksum;
+    lastChecksum = s.info.checksum;
+    schedule = s;
+    delete schedule.speakers; // Save some memory.
+    draw();
+  });
 
+  var currentSession = "initial";
+  function draw() {
+    if (!schedule) {
+      console.warn("No schedule available");
+      return;
+    }
     var today = schedule.schedule[schedule.info.currentDay];
     if (!today) {
-      console.error(`Can't find schedule`);
+      console.error(`Can't find schedule for today`);
     }
     var timeOfDay = getTimeOfDay();
     var startTimes = Object.keys(today);
-    console.info("start times today", startTimes);
     var current = startTimes[0];
     for (start of startTimes) {
       if (Number(start) >= timeOfDay) {
@@ -36,10 +50,21 @@ navigator.serviceWorker.register("/sw.js");
         break;
       }
     }
-    console.info("Current start time", start);
     var sessions = today[current];
-    console.info("sessions", sessions, today);
     var session = sessions.find(s => s.trackId == trackId && s.who != "all");
-    console.info("current session", session != null, session);
-  });
+    if (currentSession == JSON.stringify(session)) {
+      return;
+    }
+    currentSession = JSON.stringify(session);
+    console.info(
+      "current session changed",
+      start,
+      session == null ? "out of session" : "in session",
+      session
+    );
+    var outOfSession = document.getElementById("out-of-session");
+    outOfSession.style.display = session ? "none" : "block";
+  }
+
+  setInterval(draw, 5 * 1000);
 })();
