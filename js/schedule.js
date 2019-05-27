ccef084625c126a51d8ad6dfef59e2370444326c
Malte Ubl <malte.ubl@gmail.com>
2019-05-27 11:19:53 +0200

    Switch to manual control only.

--- js/schedule.js
+++ js/schedule.js
@@ -11,26 +11,18 @@ var overriddenTimeOfDay = null;
     return Number(d.getHours() + "." + d.getMinutes());
   }
 
-  var lastChecksum = "";
-  var schedule;
   var trackControl;
   var trackId = document.documentElement.getAttribute("trackid");
   if (!trackId) {
     throw new Error(`Can't find trackid`);
   }
-  poll("https://2019.jsconf.eu/schedule.json", function(url, text) {
-    var s = JSON.parse(text);
-    if (lastChecksum == s.info.checksum) {
-      console.info("Schedule checksum unchanged");
+
+  var lastTrackControl;
+  poll("/track-control.json", function(url, text) {
+    if (text == lastTrackControl) {
       return;
     }
-    lastChecksum = s.info.checksum;
-    schedule = s;
-    delete schedule.speakers; // Save some memory.
-    draw();
-  });
-
-  poll("/track-control.json", function(url, text) {
+    lastTrackControl = text;
     var t = JSON.parse(text);
     trackControl = t;
     draw();
@@ -38,52 +30,22 @@ var overriddenTimeOfDay = null;
 
   var currentSession = "initial";
   function draw() {
-    if (!schedule) {
-      console.warn("No schedule available");
-      return;
-    }
     if (!trackControl) {
       console.warn("No track control available");
       return;
     }
-    var today = schedule.schedule[schedule.info.currentDay];
-    if (!today) {
-      console.error(`Can't find schedule for today`);
-    }
     var timeOfDay = getTimeOfDay();
-    var startTimes = Object.keys(today);
-    var current = startTimes[0];
-    for (start of startTimes) {
-      if (Number(start) <= timeOfDay) {
-        current = start;
-      }
-      if (Number(start) > timeOfDay) {
-        break;
-      }
+    var control = trackControl[trackId];
+
+    var nextOrCurrentSessionIsInFuture = false;
+    if (control.nextOrCurrentSessionStartTime > timeOfDay) {
+      nextOrCurrentSessionIsInFuture = true;
     }
-    var sessions = today[current];
-    var session = sessions.find(s => s.trackId == trackId && s.who != "all");
-    var control = trackControl[trackId] || {};
-    if (control.nextOrCurrentSessionStartTime < timeOfDay) {
-      // If the next or current session is not in the future
-      // we assume it is still in session.
-      session = {
-        manualOverride: control.nextOrCurrentSessionStartTime
-      };
-    }
-    if (currentSession == JSON.stringify(session)) {
-      return;
-    }
-    currentSession = JSON.stringify(session);
-    console.info(
-      "current session changed",
-      start,
-      session == null ? "out of session" : "in session",
-      session
-    );
     var outOfSession = document.getElementById("out-of-session");
-    outOfSession.style.display = session ? "none" : "block";
+    outOfSession.style.display = nextOrCurrentSessionIsInFuture
+      ? "block"
+      : "none";
   }
 
-  setInterval(draw, 2 * 1000);
+  setInterval(draw, 1 * 1000);
 })();
